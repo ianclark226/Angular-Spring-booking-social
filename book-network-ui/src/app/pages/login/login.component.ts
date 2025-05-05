f90e0efd266841dbe93cef147b92dd775ed1745a
Ian Clark <ianclark226@gmail.com>
2025-05-05 09:49:30 -0400

    fixed FormsModule

--- book-network-ui/src/app/pages/login/login.component.ts
+++ book-network-ui/src/app/pages/login/login.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { AuthenticationRequest } from '../../services/models/authentication-request';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    standalone: true, // ✅ Make it standalone
+    imports: [CommonModule, FormsModule], // ✅ Add FormsModule here
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
